7ffcee3066cc3994111c947a77f0028e571c25aa
Torjuman <torjumanrimon7@gmail.com>
2022-02-14 21:06:16 +0600

    Second DOM Project completed

---
+++ src/02 Generate Hex Colour/app.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+  main();
+};
+
+function main() {
+  const rootDiv = document.getElementById("root");
+  const btn = document.getElementById("btn");
+  const outputCode = document.getElementById("output");
+
+  btn.addEventListener("click", function () {
+    let changeColour = generateHexColour();
+    rootDiv.style.backgroundColor = changeColour;
+    outputCode.value = changeColour;
+  });
+}
+function generateHexColour() {
+  let red = parseInt(Math.random() * 255);
+  let green = parseInt(Math.random() * 255);
+  let blue = parseInt(Math.random() * 255);
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
